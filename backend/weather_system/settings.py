9756808352e41aad4f487c529a65ed9e0415f75e
SAILESH S <sailesh25008@gmail.com>
2025-12-19 22:56:09 +0530

    Fix ALLOWED_HOSTS

--- backend/weather_system/settings.py
+++ backend/weather_system/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)$nf@u_c%+^b(p30o8uv_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-ALLOWED_HOSTS.append('.onrender.com') # Explicitly allow Render domains
+ALLOWED_HOSTS = ['*'] # Allow all hosts to prevent 500 errors on Render
 
 # Application definition
 
